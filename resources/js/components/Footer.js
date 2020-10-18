862668d87f0f54b5f81a77ff234c2fdb1cb7e876
raman <ramanvellore2018@gmail.com>
2020-10-18 22:28:08 +0530

    f commit

---
+++ resources/js/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Footer(){
+	return(
+		<div>
+		This is Footer
+		</div>
+
+		);
+}
+
+
+
+export default Footer;
---
+++ resources/js/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Home(){
+	return(
+		<div>
+		This is Home
+		</div>
+
+		);
+}
+
+export default Home;
